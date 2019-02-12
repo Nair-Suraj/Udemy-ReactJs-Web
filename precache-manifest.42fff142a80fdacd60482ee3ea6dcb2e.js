8ca4c9bdf25c0c2d05c6d821c6b5ad5fd85db004
suraj <surajnair0712@gmail.com>
2019-02-12 17:55:57 +0530

    Updates

---
+++ precache-manifest.42fff142a80fdacd60482ee3ea6dcb2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "89f6022c23a43f8aecd2",
+    "url": "/Udemy-ReactJs-Web/static/css/main.137fc074.chunk.css"
+  },
+  {
+    "revision": "89f6022c23a43f8aecd2",
+    "url": "/Udemy-ReactJs-Web/static/js/main.89f6022c.chunk.js"
+  },
+  {
+    "revision": "845eb8be2839c427e119",
+    "url": "/Udemy-ReactJs-Web/static/js/runtime~main.845eb8be.js"
+  },
+  {
+    "revision": "7c80dc727ef0200a4dcd",
+    "url": "/Udemy-ReactJs-Web/static/css/2.446b73bf.chunk.css"
+  },
+  {
+    "revision": "7c80dc727ef0200a4dcd",
+    "url": "/Udemy-ReactJs-Web/static/js/2.7c80dc72.chunk.js"
+  },
+  {
+    "revision": "30394a819bdb4171c62a79a1f34cf291",
+    "url": "/Udemy-ReactJs-Web/index.html"
+  }
+];
